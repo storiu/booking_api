eb743ffef00dd004fed00c1cd110877fdc4833f2
RedCarrot0803 <redcarrot0803@gmail.com>
2021-05-27 18:24:00 -0700

    feat:jwt Guard implement

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
-
-import { AuthService } from './auth.service';
-import { UsersModule } from '../users/users.module';
-import { LocalStrategy } from '../guards/local.strategy';
 import { PassportModule } from '@nestjs/passport';
+
+import { UsersModule } from '../users/users.module';
+import { AuthService } from './auth.service';
+import { LocalStrategy } from '../guards/local.strategy';
 import { jwtConstants } from '../common/constants/constants';
+import { JwtStrategy } from '../guards/jwt.strategy';
 
 @Module({
   imports: [
@@ -13,10 +14,18 @@ import { jwtConstants } from '../common/constants/constants';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s'},
-    })
+      signOptions: { expiresIn: '60s' },
+    }),
   ],
-  providers: [AuthService, LocalStrategy],
-  exports: [AuthService, LocalStrategy],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+  ],
+  exports: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy
+  ],
 })
 export class AuthModule {}
