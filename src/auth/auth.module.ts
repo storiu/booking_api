5cff215d32d2243ecaa21f8f4ba51bc03a0f938d
RedCarrot0803 <redcarrot0803@gmail.com>
2021-05-27 17:48:11 -0700

    feat: implement jwt sign

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
 import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from '../guards/local.strategy';
 
 @Module({
-  imports: [
-    UsersModule
-  ],
-  providers: [
-    AuthService,
-    LocalStrategy
-  ],
-  exports: [AuthService]
+  imports: [UsersModule, JwtModule],
+  providers: [AuthService, LocalStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
